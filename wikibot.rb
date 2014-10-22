2170ac7aba6162e9ad4d5f6aa78bbe4e9ebfeee3
Caleb Smith <caleb@calebsmith.net>
2014-10-21 23:59:55 -0400

    needed to change the command prefix

--- wikibot.rb
+++ wikibot.rb
@@ -19,17 +19,17 @@ bot = Cinch::Bot.new do
   on :message, "hello" do |m|
     m.reply "Hello, #{m.user.nick}"
   end
-  on :message, /^!whois (.+)$/ do |m, target|
+  on :message, /^.whois (.+)$/ do |m, target|
   user = User(target)
   m.reply "%s is named %s and connects from %s" % [user.nick, user.name, user.host]
   end
 
-  on :message, /^!ip (.+)$/ do |m, target|
+  on :message, /^.ip (.+)$/ do |m, target|
   user = User(target)
   m.reply "%s" % [user.host]
   end
 
-  on :message, /^!wiki op add (.+)$/ do |m, target|
+  on :message, /^.wiki op add (.+)$/ do |m, target|
   user = User(target)
     if chanops.include?(m.user.nick)
       ops.push('{"username" : "%s", "fcip" : "%s"}' % [user.name, user.host])
@@ -39,13 +39,13 @@ bot = Cinch::Bot.new do
     end
   end
 
-  on :message, "!wiki list ops" do |m|
+  on :message, ".wiki list ops" do |m|
   m.reply "Listing:"
     for obj in ops do
       m.reply "%s" % [obj]
     end
   end
-  on :message, "!wiki flush" do |m|
+  on :message, ".wiki flush" do |m|
   File.open("persist.txt", "w+") do |f|
   ops.each { |element| f.puts(element) }
   end
